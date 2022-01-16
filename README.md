b4b80b7b527e6b58de8f1ddb8545dd38a98f7cd7
Leonardo Werner Schulz <leonardows@gmail.com>
2022-01-16 19:57:47 -0300

    Adicionada funcionalidade de cadastrar receitas e listá-las

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # desafiopubfuture
+
+Aplicativos necessários para rodar a aplicação:
+
+1 - Eclipse IDE for Enterprise;
+2 - MySQL;
+3 - Winzip/Winrar.
+
+Passos para rodar a aplicação:
+
+1 - Baixar a pasta zipada do link: https://github.com/zalemans/desafiopubfuture/tree/master;
+2 - Descompactar os aquivos na pasta do seu workdspace do Eclipse;
+3 - Abrir o MySQL WorkBench;
+4 - Colocar o conteúdo do arquivo "dbdesafio.txt" na aba query;
+5 - Selecionar todo o texto e executar ele;
+6 - Atualizar os SCHEMAS e confirmar que o banco de dados dbdesafio foi criado;
+7 - Abrir o Eclipse IDE for Enterprise;
+8 - Abrir o projeto;
+9 - Expandir a pasta desafiopubfuture, em seguida a pasta src e a pasta application;
+10 - Clicar com o botão direito em cima do aquivo Program.java, selecionar o submenu "Run as" e em seguida "Java Application".
--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,11 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
 import java.util.Scanner;
 
 import db.DB;
@@ -8,6 +13,8 @@ import db.DB;
 public class Program {
 
 	static Scanner sc = new Scanner(System.in);
+	static Connection conn = DB.getConnection();
+	
 	
 	public static void main(String[] args) {
 		
@@ -15,7 +22,7 @@ public class Program {
 		
 		int opcao1 = 0;
 		
-		//Connection conn = DB.getConnection();
+		
 		
 		 do {
 			switch (opcao1) {
@@ -51,76 +58,177 @@ public class Program {
 	}
 
 	// ------------------------------------------------------------------------------
-	// Menu e sub-Menus das Receitas:
-	
-	static void menuReceitas() {
+		// Menu e sub-Menus das Receitas:
 		
+		static void menuReceitas() {
+			
+			
+			
+			int opcaoReceitas = 0;
+			
+			do {
+				switch (opcaoReceitas) {
+					case 1: cadReceitas();
+							break;
+		/*			case 2: editReceitas();
+							break;
+					case 3: remReceitas();
+							break;
+			*/		case 4: listReceitas();
+							break;
+				/*	case 5: listTotReceitas();
+							break; */
+					default: System.out.println("Opção inválida. Opções válidas de 1 até 6. Você digitou: " + opcaoReceitas);
+							break;
+				} 
+			System.out.println("Menu -- Receitas --");
+			System.out.println("Seguem as funções disponíveis:");
+			System.out.println("1 - Cadastrar Receitas;");
+			System.out.println("2 - Editar Receitas;");
+			System.out.println("3 - Remover Receitas;");
+			System.out.println("4 - Listar Receitas;");
+			System.out.println("5 - Listar Total de Receitas;");
+			System.out.println("6 - Voltar ao Menu Anterior.");
+			opcaoReceitas = sc.nextInt();
+			} while (opcaoReceitas != 6);
+			
+			
+		}
 		
+		static void cadReceitas() {
 		
-		int opcaoReceitas = 0;
-		
-		do {
-			switch (opcaoReceitas) {
-				case 1: cadReceitas();
+			System.out.println("Menu -- Cadastro de Receita --");
+			System.out.print("Cadastrar o valor da receita: ");
+			double receita = sc.nextDouble();
+			sc.nextLine();
+			System.out.print("Data de recebimento (aaaa-mm-dd): ");
+			String dataRecebimento = sc.nextLine();
+			
+			System.out.print("Data de recebimento esperado (aaaa-mm-dd): ");
+			String dataRecebimentoEsperado = sc.nextLine();
+			
+			System.out.print("Descrição do recebimento: ");
+			String descicaoRecebimento = sc.nextLine();
+			
+			System.out.println("Tipo de conta:");
+			System.out.println("1 - Corrente");
+			System.out.println("2 - Poupança");
+			System.out.println("3 - Carteira");
+			System.out.print("Opção: ");
+			int opcaoContaRecebimento = sc.nextInt();
+			String contaRecebimento;
+			switch (opcaoContaRecebimento) {
+			case 1: contaRecebimento = "Corrente";
+					break;
+			case 2: contaRecebimento = "Poupança";
+					break;
+			case 3: contaRecebimento = "Carteira";
+					break;
+			default: contaRecebimento = "Outra";
+					break;
+			}
+			
+			
+			
+			int tipoDeReceita;
+			String tipoDeReceitaS;
+			System.out.println("Tipo da Receita:");
+			System.out.println("1 - Salário;");
+			System.out.println("2 - Presente;");
+			System.out.println("3 - Prêmio;");
+			System.out.println("4 - Outros.");
+			System.out.print("Opção: ");
+			tipoDeReceita = sc.nextInt();
+			switch (tipoDeReceita) {
+				case 1: tipoDeReceitaS = "Salário";
 						break;
-	/*			case 2: editReceitas();
+				case 2: tipoDeReceitaS = "Presente";
 						break;
-				case 3: remReceitas();
+				case 3: tipoDeReceitaS = "Prêmio";
 						break;
-				case 4: listReceitas();
+				default: tipoDeReceitaS = "Outros";
 						break;
-				case 5: listTotReceitas();
-						break; */
-				default: System.out.println("Opção inválida. Opções válidas de 1 até 6. Você digitou: " + opcaoReceitas);
-						break;
-			} 
-		System.out.println("Menu -- Receitas --");
-		System.out.println("Seguem as funções disponíveis:");
-		System.out.println("1 - Cadastrar Receitas;");
-		System.out.println("2 - Editar Receitas;");
-		System.out.println("3 - Remover Receitas;");
-		System.out.println("4 - Listar Receitas;");
-		System.out.println("5 - Listar Total de Receitas;");
-		System.out.println("6 - Voltar ao Menu Anterior.");
-		opcaoReceitas = sc.nextInt();
-		} while (opcaoReceitas != 6);
-		
-		
-	}
-	
-	static void cadReceitas() {
-	
-		System.out.println("Menu -- Cadastro de Receita --");
-		System.out.print("Cadastrar o valor da receita: ");
-		double receita = sc.nextDouble();
-		sc.nextLine();
-		System.out.print("Data de recebimento (dd/mm/aaaa): ");
-		String dataRecebimento = sc.nextLine();
-		
-		System.out.print("Data de recebimento esperado (dd/mm/aaaa): ");
-		String dataRecebimentoEsperado = sc.nextLine();
-		
-		System.out.print("Descrição do recebimento: ");
-		String descicaoRecebimento = sc.nextLine();
-		
-		System.out.print("Conta: ");
-		String contaRecebimento = sc.nextLine();
-		
-		int tipoDeReceita;
-		
-		System.out.println("Tipo da Receita: (salário, presente, prêmio, outros): ");
-		System.out.println("1 - Salário;");
-		System.out.println("2 - Presente;");
-		System.out.println("3 - Prêmio;");
-		System.out.println("4 - Outros.");
-		tipoDeReceita = sc.nextInt();
+			}
+			PreparedStatement st = null;
+			try {
 				
-		System.out.println("Receita cadastrada.");
+				conn = DB.getConnection();
+				
+				st = conn.prepareStatement(
+						"INSERT INTO receitas "
+						+ "(valor, dataRecebimento, dataRecebimentoEsperado, descricao, conta, tipoReceita) "
+						+ "VALUES "
+						+ "(?, ?, ?, ?, ?, ?)",
+						Statement.RETURN_GENERATED_KEYS);
+				
+				st.setDouble(1, receita);
+				st.setString(2, dataRecebimento + " 00:00:00");
+				st.setString(3, dataRecebimentoEsperado + " 00:00:00");
+				st.setString(4, descicaoRecebimento);
+				st.setString(5, contaRecebimento);
+				st.setString(6, tipoDeReceitaS);
+				
+				int rowsAffected = st.executeUpdate();
+				
+				if (rowsAffected > 0 ) {
+					ResultSet rs = st.getGeneratedKeys();
+					while (rs.next()) {
+						int id = rs.getInt(1);
+						System.out.println("Receita cadastrada. Código do Id : " + id);
+					}
+				}
+				else {
+					System.out.println("No rows affected!");
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				
+			}
+			
+			finally {
+				DB.closeStatement(st);
+				DB.closeConnection();
+			}		
+			
+			
+				
+		
+		}
 	
+	// ------------------------------------------------------------------------------
+	// Listas receitas:
 	
-	}
-	
-	
+		static void listReceitas() {
+		
+			Connection conn = null;
+			Statement st = null;
+			ResultSet rs = null;
+				
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement();
+			
+			
+			rs = st.executeQuery("select * from receitas");
+			double totalReceitas = 0;
+			while (rs.next()) {
+				System.out.println("Id:" + rs.getInt("id") + ", Valor: " + rs.getDouble("valor") + ", Data Recebimento: " + rs.getString("dataRecebimento") + ", Data Recebimento Esperado: " + rs.getString("dataRecebimentoEsperado") + ", Descrição: " + rs.getString("descricao") + ", Conta: " + rs.getString("conta") + ", Tipo Receita: " + rs.getString("tipoReceita") + ".");
+				totalReceitas += rs.getDouble("valor");
+				}
+			System.out.println("Valor total das Receitas: " + totalReceitas);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+			
+			}
+		}
 	
 	
 	// ------------------------------------------------------------------------------	
@@ -171,5 +279,6 @@ public class Program {
 		
 		
 	}
+	
 
 }
